86bb7994e279c4e4719a4724ceb422499fd1f6b5
xbaldiq <iqbaldhiaa@gmail.com>
2024-01-30 10:13:04 +0700

    chore: add and setup tailwind

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,6 +1,11 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import "./Tab1.css";
 
 const Tab1: React.FC = () => {
   return (
@@ -10,13 +15,20 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent fullscreen className="relative">
+        <div className="text-center absolute left-0 right-0 top-1/2 transform -translate-y-1/2 flex justify-center items-center">
+          <div className="border border-gray-100 rounded-xl shadow-2xl p-8 bg-gradient-to-r from-green-400 via-blue-900 to-blue-400 text-white max-w-lg">
+            <h1 className="text-2xl font-bold">
+              Using TailwindCSS in Ionic with React
+            </h1>
+            <p className="mt-4">
+              This is an example of how you can use{" "}
+              <span className="font-bold">TailwindCSS</span> in an{" "}
+              <span className="font-bold">Ionic</span> application using{" "}
+              <span className="font-bold">React</span> framework.
+            </p>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
